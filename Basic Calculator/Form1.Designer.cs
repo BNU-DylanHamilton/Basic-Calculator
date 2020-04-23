72223ebd68cdda764424c61b8c66b6b9506eac25
Dylan Hamilton <dylan5437@hotmail.co.uk>
2020-04-23 17:52:55 +0100

    Add project files.

---
+++ Basic Calculator/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Basic_Calculator
+{
+    partial class calculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.firstNumberLabel = new System.Windows.Forms.Label();
+            this.secondNumberLabel = new System.Windows.Forms.Label();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.outcomeLabel = new System.Windows.Forms.Label();
+            this.powerButton = new System.Windows.Forms.Button();
+            this.averageButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(651, 59);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(111, 41);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addNumbers);
+            // 
+            // subtractButton
+            // 
+            this.subtractButton.Location = new System.Drawing.Point(651, 123);
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.Size = new System.Drawing.Size(111, 41);
+            this.subtractButton.TabIndex = 1;
+            this.subtractButton.Text = "Subtract";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            this.subtractButton.Click += new System.EventHandler(this.subtractNumbers);
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(651, 187);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(111, 41);
+            this.multiplyButton.TabIndex = 2;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyNumbers);
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(651, 248);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(111, 41);
+            this.divideButton.TabIndex = 3;
+            this.divideButton.Text = "Divide";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.divideNumbers);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(53, 373);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(111, 41);
+            this.quitButton.TabIndex = 4;
+            this.quitButton.Text = "Quit";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.closeApplication);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(30, 13);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(332, 45);
+            this.titleLabel.TabIndex = 5;
+            this.titleLabel.Text = "Simple Calculator";
+            // 
+            // firstNumberLabel
+            // 
+            this.firstNumberLabel.AutoSize = true;
+            this.firstNumberLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNumberLabel.Location = new System.Drawing.Point(35, 123);
+            this.firstNumberLabel.Name = "firstNumberLabel";
+            this.firstNumberLabel.Size = new System.Drawing.Size(129, 23);
+            this.firstNumberLabel.TabIndex = 6;
+            this.firstNumberLabel.Text = "First Number:";
+            // 
+            // secondNumberLabel
+            // 
+            this.secondNumberLabel.AutoSize = true;
+            this.secondNumberLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondNumberLabel.Location = new System.Drawing.Point(34, 188);
+            this.secondNumberLabel.Name = "secondNumberLabel";
+            this.secondNumberLabel.Size = new System.Drawing.Size(156, 23);
+            this.secondNumberLabel.TabIndex = 7;
+            this.secondNumberLabel.Text = "Second Number:";
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsLabel.Location = new System.Drawing.Point(35, 263);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(71, 23);
+            this.resultsLabel.TabIndex = 8;
+            this.resultsLabel.Text = "Result:";
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNumberTextBox.Location = new System.Drawing.Point(208, 107);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(202, 53);
+            this.firstNumberTextBox.TabIndex = 9;
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondNumberTextBox.Location = new System.Drawing.Point(208, 179);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(202, 53);
+            this.secondNumberTextBox.TabIndex = 10;
+            // 
+            // outcomeLabel
+            // 
+            this.outcomeLabel.BackColor = System.Drawing.Color.Blue;
+            this.outcomeLabel.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outcomeLabel.Location = new System.Drawing.Point(209, 248);
+            this.outcomeLabel.Name = "outcomeLabel";
+            this.outcomeLabel.Size = new System.Drawing.Size(201, 53);
+            this.outcomeLabel.TabIndex = 11;
+            // 
+            // powerButton
+            // 
+            this.powerButton.Location = new System.Drawing.Point(651, 308);
+            this.powerButton.Name = "powerButton";
+            this.powerButton.Size = new System.Drawing.Size(111, 41);
+            this.powerButton.TabIndex = 12;
+            this.powerButton.Text = "Power";
+            this.powerButton.UseVisualStyleBackColor = true;
+            this.powerButton.Click += new System.EventHandler(this.powerNumbers);
+            // 
+            // averageButton
+            // 
+            this.averageButton.Location = new System.Drawing.Point(651, 373);
+            this.averageButton.Name = "averageButton";
+            this.averageButton.Size = new System.Drawing.Size(111, 41);
+            this.averageButton.TabIndex = 13;
+            this.averageButton.Text = "Average";
+            this.averageButton.UseVisualStyleBackColor = true;
+            this.averageButton.Click += new System.EventHandler(this.averageNumbers);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(217, 373);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(111, 41);
+            this.clearButton.TabIndex = 14;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearTextBoxes);
+            // 
+            // calculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.averageButton);
+            this.Controls.Add(this.powerButton);
+            this.Controls.Add(this.outcomeLabel);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.secondNumberLabel);
+            this.Controls.Add(this.firstNumberLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.subtractButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "calculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Basic Calculator";
+            this.Load += new System.EventHandler(this.startUpScreen);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label firstNumberLabel;
+        private System.Windows.Forms.Label secondNumberLabel;
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.Label outcomeLabel;
+        private System.Windows.Forms.Button powerButton;
+        private System.Windows.Forms.Button averageButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Basic Calculator/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Calculator
+{
+    public partial class calculatorForm : Form
+    {
+        private double firstNumber, secondNumber;
+
+        public calculatorForm()
+        {
+            InitializeComponent();
+            outcomeLabel.Text = "";
+        }
+
+        private void closeApplication(object sender, EventArgs e)
+        {
+            quitScreen QScreen = new quitScreen();
+            QScreen.ShowDialog();
+        }
+
+        private void subtractNumbers(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            outcomeLabel.Text = Convert.ToString(firstNumber - secondNumber);
+        }
+
+        private void multiplyNumbers(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            outcomeLabel.Text = Convert.ToString(firstNumber * secondNumber);
+        }
+
+        private void divideNumbers(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            if(secondNumber == 0)
+            {
+                MessageBox.Show("You should NOT divide by ZERO!");
+            }
+            else
+            {
+                outcomeLabel.Text = Convert.ToString(firstNumber / secondNumber);
+            }
+        }
+
+        private void startUpScreen(object sender, EventArgs e)
+        {
+            splashScreenForm SScreen = new splashScreenForm();
+            SScreen.ShowDialog();
+            System.Threading.Thread.Sleep(2000);
+            SScreen.Close();
+        }
+
+        private void clearTextBoxes(object sender, EventArgs e)
+        {
+            firstNumberTextBox.Text = "";
+            secondNumberTextBox.Text = "";
+        }
+
+        private void powerNumbers(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            outcomeLabel.Text = Convert.ToString(Math.Pow(firstNumber, secondNumber));
+        }
+
+        private void averageNumbers(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            outcomeLabel.Text = Convert.ToString((firstNumber + secondNumber) / 2);
+        }
+
+        private void addNumbers(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            outcomeLabel.Text = Convert.ToString(firstNumber + secondNumber);
+        }
+    }
+}
